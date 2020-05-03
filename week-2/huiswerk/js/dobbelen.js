ef750743255709df08cfbc475438549b6f2087cf
Job Groen <jobgroen98@gmail.com>
2020-05-03 22:53:31 +0200

    Week 2 - Inleiding programmeren
    Les: console.log()

---
+++ week-2/huiswerk/js/dobbelen.js
@@ -0,0 +1,15 @@
+console.log('programmeren moet je vaak even puzzelene');
+
+document.write('programmeren moet je leren door te doen en niet te kopieeren');
+document.write('Ik zal vaak gefrustreerd raken en tegen een muur oplopen!');
+document.write('Dit komt niet doordat ik het niet kan, maar of programmeren wel bij mij hoort');
+document.write('Wanneer het mij toch lukt en ik alle bugs heb verpletterd, voel ik me goed');
+document.write('Tot dat ik weer tegen een bug aanloop want dat is een uitdaging<br>');
+
+var naamHeld = "Job Groen";
+
+var aantalBugs = 8 ;
+var mijnGevoel = "super lekker man.";
+var specialPower = "intilligentie";
+
+document.write('<br>hallo mijn naam is ' + naamHeld + ' ik ben een js held, ik zal ' + aantalBugs + ' bugs troteren misschien zelf ' + aantalBugs*10 + ' maar door mijn ' + specialPower + ' zal ik mijn tegenslagen overwinnen en voel mij daardoor ' + mijnGevoel + '.');
