b7713104672426a4a4ec8c44cdd74c6513240fca
Job Groen <jobgroen98@gmail.com>
2020-05-03 22:42:37 +0200

    Week 2 - Inleiding programmeren
    Les: Eilanden ( 2 manieren )

---
+++ week-2/huiswerk/js/script.js
@@ -0,0 +1,28 @@
+// Manier 1 Eilanden
+console.log('test of koppeling werkt.');
+
+var eilanden = ['Terschelling', 'Ameland', 'Schiermonnikoog', 'Rottumerplaat'];
+var aantalEilanden = 4;
+
+console.log(eilanden[0] + ' en ' + eilanden[3]);
+
+
+
+// Manier 2 Eilanden 
+console.log('test of koppeling werkt.');
+
+var eilanden = ['Terschelling', 'Ameland', 'Schiermonnikoog', 'Rottumerplaat'];
+var aantalEilanden = 4;
+
+
+function Volgorde() {  
+ let eerste=eilanden[0];  
+ let laatste=eilanden[eilanden.length-1];  
+
+ console.log("Het eerste eiland is " + eerste + " En het laatste eiland is " + laatste);
+
+ }  
+
+Volgorde();  
+
+
