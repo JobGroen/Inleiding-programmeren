021383cbdbab0a24759b7d52f8db50c58c9250f2
Job Groen <jobgroen98@gmail.com>
2020-05-07 00:02:57 +0200

    Week 2 - Dobbelen Bonus
    Bonus opdracht dobbelen met animatie en keyframes

--- week-2/dobbelen-v-2/script.js
+++ week-2/dobbelen-v-2/script.js
@@ -1,14 +1,20 @@
 function werp(){
 
+    document.querySelector('img').classList.add("throw");
+    
+    // var audio = new Audio('sound.mp3');
+    // audio.play();
+    
     var randomGetal = Math.random()*6;
     var aantalOgen = Math.floor(randomGetal) + 1;
-
+    
     plaatjesArray = ['dice1.png', 'dice2.png', 'dice3.png','dice4.png', 'dice5.png', 'dice6.png'];
-
+    
     var dobbelsteenAfbeelding = document.querySelector("img").src="img/dice" + aantalOgen + ".png";
-
+    
     console.log(aantalOgen);
     console.log(dobbelsteenAfbeelding);
+    
 }
 
 document.querySelector("img").addEventListener("click", werp);
