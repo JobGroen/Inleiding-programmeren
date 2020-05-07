64003a7c2eeee8fd9a9d9a7d7db956cde3e3a4c7
Job Groen <jobgroen98@gmail.com>
2020-05-07 02:12:09 +0200

    Week 3 Bonus
    Oppervlakte bereken bonus:
    omtrek cirkel - oppervlakte cirkel en inhoud van een bal

--- week-3/oppervlakte-berekenen/script.js
+++ week-3/oppervlakte-berekenen/script.js
@@ -29,3 +29,34 @@ function inhoud(breedte, hoogte, lengte){
 var total = inhoud(10,10,10);
 
 console.log("De inhoud is: " + total + " cm3.");
+
+
+
+// Omtrek berekenen
+function omtrekBerekenen(straal, pie){
+    return straal * 2 * pie;
+}
+
+var omtrekcirkel = omtrekBerekenen(10, 3.14);
+
+console.log("De omtrek van de cirkel is " + Math.round(omtrekcirkel) + " cm2");
+
+
+// oppervlakte berekenen
+function oppervlakteCirkel(straal, pie){
+    return straal * straal * pie;
+}
+
+var oppcirkel = oppervlakteCirkel(10, 3.14);
+
+console.log("De oppervlakte van de cirkel is " + Math.round(oppcirkel) + " cm2");
+
+
+// Inhoud bol berekenen
+function bolBerekenen(pie, straal) {
+    return 4 / 3 * pie * straal * straal * straal;
+}
+
+var balInhoud = bolBerekenen(10, 3.14);
+
+console.log("De inhoud van de bol is " + Math.round(balInhoud) + " cm3");
