e4e66a9b76fea0f1afecf21826e140b668debef7
Job Groen <jobgroen98@gmail.com>
2020-05-07 10:57:37 +0200

    Week 2 - Dobbelsteen Update

--- week-2/dobbelen-v-1/script.js
+++ week-2/dobbelen-v-1/script.js
@@ -1,11 +1,8 @@
-// De reden waarom je geen ceil gebruikt maar floor is dat floor afrond naar beneden.. stel je hebt 6.05 wil je niet
-// hij naar 7 afrond, want een dobbelsteen heeft geen 7 ogen, maar 6.
-
 // Je genereert een getal tussen 0 en 6
 var randomGetal = Math.random()*6;
 
 // Variable for aantal ogen, omdat je geen 0 op een dobbelstene hebt doe je bij math.floor + 1 
-var aantalOgen = Math.floor(randomGetal) + 1;
+var aantalOgen = Math.ceil(randomGetal);
 
 // Stap 3
 plaatjesArray = ['dice1.png', 'dice2.png', 'dice3.png','dice4.png', 'dice5.png', 'dice6.png'];
