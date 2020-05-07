734c374f080d26acad46c0e9cb4c08250dfaa875
Job Groen <jobgroen98@gmail.com>
2020-05-07 10:51:27 +0200

    Week 3 - Debugging update
    -

--- week-3/Debugging opdracht/scripts/debugging.js
+++ week-3/Debugging opdracht/scripts/debugging.js
@@ -10,6 +10,29 @@
 //Dit script begroet mijn collega's en toont dit in de browser!
 
 //array collega's
+var collegas = ('Walter', 'Laura', 'Sonja');
+var content = document.querySelectorAll('body')[0];
+
+
+
+//deze functie begroet mijn collega's
+function groet(persoon) {
+    var naam = persoon.toLowerCase();
+    var printTekst = 'Hallo' + naam
+    content.textContent = content.textContent + printTekst;
+}
+
+//hier roep ik drie keer de functie aan
+groet(collegas[1]);
+groet(collegas[2]);
+groet(collegas[3]);
+
+
+
+
+// DIT IS GEDEBUGGED
+
+//array collega's
 var collegas = ['Walter', 'Laura', 'Sonja'];
 var content = document.querySelectorAll('p')[0];
 
@@ -20,18 +43,8 @@ function greeting(persons){
     var name = persons;
     var print = " Hallo " + name;
 
-    content.textContent = content.textContent + print;
+    content.textContent = print;
 }
 
-
-
-// function groet(persoon) {
-//     var naam = persoon;
-//     var printTekst = ' Hallo ' + naam;
-//     content.textContent = content.textContent + printTekst;
-// }
-
 //hier roep ik drie keer de functie aan
-groet(collegas[0]);
-groet(collegas[1]);
-groet(collegas[2]);
+greeting(collegas[0] + ", " + collegas[1] + " en " +  collegas[2]);
