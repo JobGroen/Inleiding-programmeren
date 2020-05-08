aafadae8b0a4c7c24f3d9351a0ec24012be32fe2
Job Groen <jobgroen98@gmail.com>
2020-05-08 15:21:28 +0200

    Update

---
+++ utils.js
@@ -0,0 +1,23 @@
+// variable = var isDit;
+// Boolean = true or false
+// Null has no value
+// Undefined is als iets geen waarde heeft
+// Number = integers
+// String = "woord"
+
+// Array = ['1', '2', '3', '4'];
+// Function ditiseenfunction(){}
+// Function met parameters = function oppervlakte(lengte, breedte){}
+
+
+// Query pakken uit html = document.querySelector('#antwoord');
+// tekst plakken in query =  antwoordTekst.textContent = 'Eng he';
+
+// Hier log je in de console wat je hebt in getyped in het formulier = console.log(document.querySelector('input').value);
+// classlist toevoegen = document.querySelector('form').classList.add("trigger_form_animation");
+
+// wanneer er op submit voert hij function uit = document.querySelector('form').addEventListener('submit',renderForm);
+
+// Je genereert een getal tussen 0 en 6 = var randomGetal = Math.random()*6;
+
+// Hoog of laag afronden = var aantalOgen = Math.ceil(randomGetal); (of math.floor)
--- week-2/dobbelen-v-2/script.js
+++ week-2/dobbelen-v-2/script.js
@@ -1,20 +1,49 @@
+// function werp(){
+
+//     document.querySelector('img').classList.add("throw");
+    
+//     // var audio = new Audio('sound.mp3');
+//     // audio.play();
+    
+//     var randomGetal = Math.random()*6;
+//     var aantalOgen = Math.floor(randomGetal) + 1;
+    
+//     plaatjesArray = ['dice1.png', 'dice2.png', 'dice3.png','dice4.png', 'dice5.png', 'dice6.png'];
+    
+//     var dobbelsteenAfbeelding = document.querySelector("img").src="img/dice" + aantalOgen + ".png";
+    
+//     console.log(aantalOgen);
+//     console.log(dobbelsteenAfbeelding);
+    
+// }
+
+// document.querySelector("img").addEventListener("click", werp);
+
+// function werp
 function werp(){
 
+    // class toevoegen
     document.querySelector('img').classList.add("throw");
-    
-    // var audio = new Audio('sound.mp3');
-    // audio.play();
-    
-    var randomGetal = Math.random()*6;
-    var aantalOgen = Math.floor(randomGetal) + 1;
-    
+
+    // plaatjes array
     plaatjesArray = ['dice1.png', 'dice2.png', 'dice3.png','dice4.png', 'dice5.png', 'dice6.png'];
     
-    var dobbelsteenAfbeelding = document.querySelector("img").src="img/dice" + aantalOgen + ".png";
+    // random getal genereren 
+    var randomGetal = Math.random()*6;
+
+    // random getal afronden
+    var aantalOgen = Math.ceil(randomGetal);
     
-    console.log(aantalOgen);
-    console.log(dobbelsteenAfbeelding);
+    // aantal ogen -1 want 6 bestaat niet in de array
+    var plaatjesNummer = aantalOgen-1;
+
+    // plaatje is random getal van de random
+    var dobbelsteenAfbeelding = plaatjesArray[plaatjesNummer];
     
+    // afbeelding linken aan getal
+    document.querySelector("img").src = "img/" + dobbelsteenAfbeelding;
 }
 
+// werp functie toepassen
 document.querySelector("img").addEventListener("click", werp);
+
